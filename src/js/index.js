eaa726e1483a0c07501639e2b6cfef00cb82b94d
Przemyslaw Duniec <pduniec@outlook.com>
2021-09-19 14:33:10 +0200

    modify all scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,59 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+console.log("Hi, I'm Przemek - nice to meet you 🚀");
 
-/* place your code below */
+fetch("https://api.github.com/users/itzygzak/repos?sort=created&direction=asc")
+  .then((res) => res.json())
+  .then((res) => {
+    const container = document.querySelector(".projects-grid--js");
+    for (let repo of res) {
+      const { description, homepage, html_url, name } = repo;
 
-console.log('HELLO 🚀')
+      const template = `<article class="project">
+        <div class="project__window">
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+        </div>
+        <div class="project__content">
+          <img src="img/github.svg" alt="" />
+          <h3 class="project__grid project__title">
+            <span class="project__label">project:</span>
+            <span>${name}</span>
+          </h3>
+          <p class="project__grid project__grid--description">
+            <span class="project__label">description:</span>
+            <span>${description}</span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">demo:</span>
+            <span
+              >&lt;<a target="_blank" 
+                rel="noopener noreferrer"
+                class="project__link"
+                href="${homepage}"
+                title="${name} - demo"
+                >see_here</a
+              >&lt;
+            </span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">github:</span>
+            <span
+              >&lt;<a
+                class="project__link"
+                href="${html_url}"
+                target="_blank" 
+                rel="noopener noreferrer"
+                title="${name} - code"
+                >source_code</a
+              >&lt;</span
+            >
+          </p>
+        </div>
+      </article>`;
+
+      container.innerHTML += template;
+    }
+  })
+  .catch((e) => console.log(e));
